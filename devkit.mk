legionus/devkit

commit ddbc4ebf1513eeab17d1cd1a11e40cb3cd0e7b5e

Mount /run and /tmp as tmpfs
/run and /tmp contains volatile ephemeral temporary files that are
cleaned up when the container exits. There is no need to store them on
the physical device, and this reduces drive writes that should prolong
the drive lifetime.

Signed-off-by: Wladmis <dev@wladmis.org>

devkit.mk
@@ -133,6 +133,8 @@ VOLUMES += $(foreach f,$(CONFFILES),$(if $(wildcard $(HOME)/$(f)),$(HOME)/$(f):/
 PODMAN_ARGS = \
 	--env=LANG=C.UTF8 \
 	--env=EDITOR=$(EDITOR) \
+	--tmpfs /run \
+	--tmpfs /tmp \
 	--tty --interactive \
 	--user='$(if $(ROOT),root,$(UID):$(GID))' \
 	--workdir='$(WORKDIR)'

commit 0021433017d2fab2592ea9f0dbe374bf0f9201b8

Fix the attachment to the container
There is no --tmpfs option for `podman container exec` because we are
attaching to an existing container.

  Error: unknown flag: --tmpfs
  See 'podman container exec --help'
  make: *** [/home/me/devkit/devkit.mk:305: run] Error 125

Fixes: ddbc4eb ("Mount /run and /tmp as tmpfs")
Signed-off-by: Alexey Gladkov <gladkov.alexey@gmail.com>

devkit.mk
@@ -133,8 +133,6 @@ VOLUMES += $(foreach f,$(CONFFILES),$(if $(wildcard $(HOME)/$(f)),$(HOME)/$(f):/
 PODMAN_ARGS = \
 	--env=LANG=C.UTF8 \
 	--env=EDITOR=$(EDITOR) \
-	--tmpfs /run \
-	--tmpfs /tmp \
 	--tty --interactive \
 	--user='$(if $(ROOT),root,$(UID):$(GID))' \
 	--workdir='$(WORKDIR)'
@@ -142,6 +140,8 @@ PODMAN_RUNTIME_ARGS = $(PODMAN_ARGS) \
 	$(addprefix --env-file=,$(ENVFILES)) \
 	$(addprefix --cap-del=,$(patsubst -%,%,$(filter -%,$(CAPS)))) \
 	$(addprefix --cap-add=,$(patsubst +%,%,$(filter +%,$(CAPS)))) \
+	--tmpfs /run \
+	--tmpfs /tmp \
 	--rm --network=host --userns=keep-id --memory=$(LIMIT_MEMORY)
 PODMAN_VOLUMES = \
 	--volume=$(GITPROJDIR):/srv/$(PROJNAME):rw,Z \